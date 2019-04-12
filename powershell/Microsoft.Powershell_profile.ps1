93994fa85a4fd21f9c1c1f7c954ed7e3d0030190
Joseph Kim <joskim@microsoft.com>
2019-04-12 10:11:39 -0700

    added devcmd function to powershell

--- powershell/Microsoft.Powershell_profile.ps1
+++ powershell/Microsoft.Powershell_profile.ps1
@@ -26,3 +26,14 @@ function emacs {
         emacsclientw.exe -n --no-wait --alternate-editor="runemacs.exe" -e '';
     }
 }
+
+# Dev Command Prompt Functionality in Powershell
+function devcmd {
+    $installationPath = vswhere.exe -prerelease -latest -property installationPath;
+    if ($installationPath -and (test-path "$installationPath\Common7\Tools\vsdevcmd.bat")) {
+        & "${env:COMSPEC}" /s /c "`"$installationPath\Common7\Tools\vsdevcmd.bat`" -no_logo && set" | foreach-object {
+            $name, $value = $_ -split '=', 2;
+            set-content env:\"$name" $value;
+        }
+    }
+}
