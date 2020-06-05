c07922413cf86030578ae3a5cf446da3912fc05d
Joseph Kim <joskim@microsoft.com>
2020-06-05 16:25:56 -0700

    avy-goto-char-timer bound to C-: and timer increased to 0.15 seconds

--- emacs/.emacs.d/lisp/init-keybindings.el
+++ emacs/.emacs.d/lisp/init-keybindings.el
@@ -18,9 +18,9 @@
 ;; Movements
 
 (global-set-key (kbd "C-;") 'avy-goto-line)
-(global-set-key (kbd "C-M-;") 'avy-goto-char-timer)
+(global-set-key (kbd "C-:") 'avy-goto-char-timer)
 ;; Sets the timeout for avy-goto-char-timer
-(setq avy-timeout-seconds 0.1)
+(setq avy-timeout-seconds 0.15)
 
 (global-set-key (kbd "C-S-n")
                 (lambda ()
