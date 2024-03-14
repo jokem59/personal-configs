91d373621dee0a5f193300093bd03f9e2c09d08b
Joe Kim <kimmjose@amazon.com>
2024-03-14 17:43:46 +0000

    [emacs] Set exec path from shell.

--- emacs/.emacs.d/lisp/init-utils.el
+++ emacs/.emacs.d/lisp/init-utils.el
@@ -24,6 +24,22 @@
 (put 'upcase-region 'disabled nil)
 (put 'downcase-region 'disabled nil)
 
+(defun set-exec-path-from-shell-PATH ()
+  "Set up Emacs' `exec-path' and PATH environment variable to match
+that used by the user's shell.
+
+This is particularly useful under Mac OS X and macOS, where GUI
+apps are not started from a shell."
+  (interactive)
+  (let ((path-from-shell (replace-regexp-in-string
+			  "[ \t\n]*$" "" (shell-command-to-string
+					  "$SHELL --login -c 'echo $PATH'"
+						    ))))
+    (setenv "PATH" path-from-shell)
+    (setq exec-path (split-string path-from-shell path-separator))))
+
+(set-exec-path-from-shell-PATH)
+
 ;; Do not check if remote files are readable
 ;; Constant checks freeze on Windows when buffers to large network files exists
 (setq recentf-keep '(file-remote-p file-readable-p))
